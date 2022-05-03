3d639b5d6ea7482dba73384268a3316b84dfe364
unknown <martin_suarez_mendez@outlook.com>
2022-05-02 20:58:57 -0500

    app done

---
+++ src/src/src/components/characters/CharacterItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import brit from "../../img/britney.jpg";
+
+const CharacterItem = ({ item }) => {
+  console.log(item, "itemmmm");
+  console.log(brit);
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <img
+            src={item.img && item.name === "Holly White" ? brit : item.img}
+            alt="item.img"
+          />
+        </div>
+        <div className="card-back">
+          <h1>{item.name}</h1>
+          <ul>
+            <li>
+              <strong>Actor Name:</strong> {item.portrayed}
+            </li>
+            <li>
+              <strong>Nickname:</strong> {item.nickname}
+            </li>
+            <li>
+              <strong>Birthday:</strong> {item.birthday}
+            </li>
+            <li>
+              <strong>Status:</strong> {item.status}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterItem;
